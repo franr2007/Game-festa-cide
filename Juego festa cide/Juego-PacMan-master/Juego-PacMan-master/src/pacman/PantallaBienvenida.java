d5d875c08a5e7445fa9c9823a29531339dddef9a
oiher <osalom2@nigul.cide>
2026-02-20 20:27:56 +0100

    fondo-cambio

--- Juego festa cide/Juego-PacMan-master/Juego-PacMan-master/src/pacman/PantallaBienvenida.java
+++ Juego festa cide/Juego-PacMan-master/Juego-PacMan-master/src/pacman/PantallaBienvenida.java
@@ -25,8 +25,8 @@ import javax.swing.border.Border;
 
 /**
  * Pantalla de bienvenida.
- *
- * @author Carlos Aguirre
+ * 
+ * Autor: Carlos Aguirre
  */
 public class PantallaBienvenida extends JFrame {
 
@@ -36,18 +36,10 @@ public class PantallaBienvenida extends JFrame {
     private JPanel panelGlobal, panelMenu;
     private JButton btnJugar, btnAcerca, btnSalir;
     private Icon imgIcono, imgPacman, imgFantasma1, imgFantasma2, imgCopa;
-    private Icon[] coleccionPortadas = {
-        new ImageIcon(getClass().getResource("/recursos/portada1.png")),
-        new ImageIcon(getClass().getResource("/recursos/portada2.png")),
-        new ImageIcon(getClass().getResource("/recursos/portada3.png")),
-        new ImageIcon(getClass().getResource("/recursos/portada4.png"))
-    };
-    private final Font fuenteTexto = new Font("Default", 1, 20);
+    private Icon[] coleccionPortadas;
+    private final Font fuenteTexto = new Font("Default", Font.BOLD, 20);
     private JLabel lblPuntuacionMaxima, lblPortada;
 
-    /**
-     * Cosntructor de la clase. Inicializa elementos y da forma a la ventana.
-     */
     public PantallaBienvenida() {
         this.crearElementos();
         this.crearDistribucion();
@@ -55,7 +47,7 @@ public class PantallaBienvenida extends JFrame {
         this.definirEstilos();
         this.eventos();
 
-        // Propiedades de la ventana.
+        // Configuración de ventana
         this.setTitle("PacMan");
         this.setResizable(false);
         this.pack();
@@ -64,120 +56,140 @@ public class PantallaBienvenida extends JFrame {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
 
-        // Cargar puntuacion si la hay.
+        // Cargar record si existe
         this.cargarPuntuacion();
     }
 
-    // Crea (instancia) todos los elementos necesarios.
+    // Crear todos los elementos
     private void crearElementos() {
         this.controlGuardado = new GuardarCargar();
         this.rand = new Random();
         this.panelGlobal = new JPanel();
         this.panelMenu = new JPanel();
-        this.btnJugar = new JButton("Jugar");
-        this.btnAcerca = new JButton("Acerca");
-        this.btnSalir = new JButton("Salir");
-        this.imgIcono = new ImageIcon(getClass().getResource("/recursos/logo.png"));
-        this.imgPacman = new ImageIcon(getClass().getResource("/recursos/pacman_dcha.png"));
-        this.imgFantasma1 = new ImageIcon(getClass().getResource("/recursos/fantasma4.png"));
-        this.imgFantasma2 = new ImageIcon(getClass().getResource("/recursos/fantasma7.png"));
-        this.imgCopa = new ImageIcon(getClass().getResource("/recursos/copa.png"));
-        this.lblPuntuacionMaxima = new JLabel();
-        this.lblPortada = new JLabel(coleccionPortadas[this.rand.nextInt(coleccionPortadas.length)]);
+
+        // Portadas
+        coleccionPortadas = new Icon[] {
+            new ImageIcon(getClass().getResource("/recursos/portada1.png")),
+            new ImageIcon(getClass().getResource("/recursos/portada2.png")),
+            new ImageIcon(getClass().getResource("/recursos/portada3.png")),
+            new ImageIcon(getClass().getResource("/recursos/portada4.png"))
+        };
+
+        // Fondo principal
+        JLabel fondoGlobal = new JLabel(new ImageIcon(getClass().getResource("/recursos/fondo-removebg-preview.png")));
+        fondoGlobal.setLayout(new BorderLayout());
+
+        // Panel de botones
+        panelMenu = new JPanel(new GridLayout(1, 3, 15, 0));
+        panelMenu.setOpaque(false);
+
+        // Botones
+        btnJugar = new JButton("Jugar");
+        btnAcerca = new JButton("Acerca");
+        btnSalir = new JButton("Salir");
+
+        panelMenu.add(btnJugar);
+        panelMenu.add(btnAcerca);
+        panelMenu.add(btnSalir);
+
+        // Agregar panelMenu al fondo
+        fondoGlobal.add(panelMenu, BorderLayout.SOUTH);
+
+        // Portada y record
+        lblPortada = new JLabel(coleccionPortadas[rand.nextInt(coleccionPortadas.length)], JLabel.CENTER);
+        lblPuntuacionMaxima = new JLabel("", JLabel.CENTER);
+
+        fondoGlobal.add(lblPortada, BorderLayout.CENTER);
+        fondoGlobal.add(lblPuntuacionMaxima, BorderLayout.NORTH);
+
+        // Agregar el fondo al panelGlobal
+        panelGlobal.setLayout(new BorderLayout());
+        panelGlobal.add(fondoGlobal, BorderLayout.CENTER);
+
+        // Iconos
+        imgIcono = new ImageIcon(getClass().getResource("/recursos/Logo_cide.png"));
+        imgPacman = new ImageIcon(getClass().getResource("/recursos/pacman_dcha.png"));
+        imgFantasma1 = new ImageIcon(getClass().getResource("/recursos/fantasma4.png"));
+        imgFantasma2 = new ImageIcon(getClass().getResource("/recursos/fantasma7.png"));
+        imgCopa = new ImageIcon(getClass().getResource("/recursos/copa.png"));
     }
 
-    // Asigna una distribucion a cada panel.
+    // Layout principal
     private void crearDistribucion() {
         this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
-        this.panelGlobal.setLayout(new BorderLayout());
-        this.panelMenu.setLayout(new GridLayout(1, 3, 15, 0));
     }
 
-    // Coloca los elementos en su respectivo panel.
+    // Colocar panelGlobal en el frame
     private void colocarElementos() {
-        this.getContentPane().add(this.panelGlobal);
-        this.panelGlobal.add(this.panelMenu, BorderLayout.SOUTH);
-        this.panelMenu.add(this.btnJugar);
-        this.panelMenu.add(this.btnAcerca);
-        this.panelMenu.add(this.btnSalir);
-        this.panelGlobal.add(lblPuntuacionMaxima, BorderLayout.NORTH);
-        this.panelGlobal.add(lblPortada, BorderLayout.CENTER);
+        this.getContentPane().add(panelGlobal);
     }
 
-    // Define todos los estilos.
+    // Estilos de botones y labels
     private void definirEstilos() {
-        this.panelGlobal.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-        this.panelGlobal.setBackground(Color.black);
-        this.panelMenu.setOpaque(false);
-        this.setIconImage(Toolkit.getDefaultToolkit().getImage(Juego.class.getResource("/recursos/icono.png")));
+        panelGlobal.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
 
         Border bordeBlanco = BorderFactory.createLineBorder(Color.white, 2);
         Color colorBoton = new Color(32, 38, 117);
-        this.btnJugar.setFont(fuenteTexto);
-        this.btnSalir.setFont(fuenteTexto);
-        this.btnJugar.setForeground(Color.white);
-        this.btnJugar.setBackground(colorBoton);
-        this.btnJugar.setBorder(bordeBlanco);
-        this.btnAcerca.setFont(fuenteTexto);
-        this.btnAcerca.setFont(fuenteTexto);
-        this.btnAcerca.setForeground(Color.white);
-        this.btnAcerca.setBackground(colorBoton);
-        this.btnAcerca.setBorder(bordeBlanco);
-        this.btnSalir.setFont(fuenteTexto);
-        this.btnSalir.setFont(fuenteTexto);
-        this.btnSalir.setForeground(Color.white);
-        this.btnSalir.setBackground(colorBoton);
-        this.btnSalir.setBorder(bordeBlanco);
 
-        // Iconos diferentes para cada botón.
-        this.btnJugar.setIcon(imgPacman);
-        this.btnAcerca.setIcon(imgFantasma1);
-        this.btnSalir.setIcon(imgFantasma2);
+        JButton[] botones = { btnJugar, btnAcerca, btnSalir };
+        Icon[] iconos = { imgPacman, imgFantasma1, imgFantasma2 };
 
-        this.btnJugar.setFocusable(false);
-        this.btnAcerca.setFocusable(false);
-        this.btnSalir.setFocusable(false);
+        for (int i = 0; i < botones.length; i++) {
+            botones[i].setFont(fuenteTexto);
+            botones[i].setForeground(Color.white);
+            botones[i].setBackground(colorBoton);
+            botones[i].setBorder(bordeBlanco);
+            botones[i].setIcon(iconos[i]);
+            botones[i].setFocusable(false);
+            botones[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
+        }
 
-        this.btnJugar.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        this.btnAcerca.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        this.btnSalir.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        lblPuntuacionMaxima.setFont(fuenteTexto);
+        lblPuntuacionMaxima.setForeground(Color.white);
 
-        // Otros
-        this.lblPuntuacionMaxima.setFont(fuenteTexto);
-        this.lblPuntuacionMaxima.setForeground(Color.white);
+        this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/recursos/fondo.png")));
     }
 
-    // Controla todos los eventos.
+    // Eventos de botones y labels
     private void eventos() {
-        this.btnJugar.addActionListener(new ActionListener() {
+        btnJugar.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent ae) {
+            public void actionPerformed(ActionEvent e) {
                 new Juego(recordAnterior);
-                PantallaBienvenida.this.dispose();
+                dispose();
             }
         });
-        this.btnAcerca.addActionListener(new ActionListener() {
+
+        btnAcerca.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent ae) {
-                JOptionPane.showMessageDialog(PantallaBienvenida.this, "Juego programado por: Carlos Aguirre en colaboracion con DaPelle y Millan.\nAutor original: Toru Iwatani (1980)\nVersion: 12/03/2019", "Acerca de", JOptionPane.INFORMATION_MESSAGE, imgIcono);
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(PantallaBienvenida.this,
+                        "Juego programado por: Carlos Aguirre en colaboracion con DaPelle y Millan.\n"
+                        + "Autor original: Toru Iwatani (1980)\nVersion: 12/03/2019"
+                        +"Este juego de puede jugar tanto con WASD o con las flechas de direccion para mover a pacman",
+                        "Acerca de", JOptionPane.INFORMATION_MESSAGE, imgIcono);
             }
         });
-        this.btnSalir.addActionListener(new ActionListener() {
+
+        btnSalir.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent ae) {
+            public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
-        this.lblPortada.addMouseListener(new MouseAdapter() {
+
+        lblPortada.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent me) {
+            public void mouseClicked(MouseEvent e) {
                 lblPortada.setIcon(coleccionPortadas[rand.nextInt(coleccionPortadas.length)]);
             }
         });
-        this.lblPuntuacionMaxima.addMouseListener(new MouseAdapter() {
+
+        lblPuntuacionMaxima.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent me) {
-                int decision = JOptionPane.showConfirmDialog(PantallaBienvenida.this, "¿Deseas restablecer el record?", "Borrar record", JOptionPane.YES_NO_OPTION);
+            public void mouseClicked(MouseEvent e) {
+                int decision = JOptionPane.showConfirmDialog(PantallaBienvenida.this,
+                        "¿Deseas restablecer el record?", "Borrar record", JOptionPane.YES_NO_OPTION);
                 if (decision == JOptionPane.OK_OPTION) {
                     controlGuardado.guardar(-1);
                     lblPuntuacionMaxima.setVisible(false);
@@ -187,16 +199,16 @@ public class PantallaBienvenida extends JFrame {
         });
     }
 
-    // Carga la puntuacion anterior si la hay.
+    // Cargar record si existe
     private void cargarPuntuacion() {
-        this.recordAnterior = this.controlGuardado.cargar();
-        if (this.recordAnterior != -1) {
-            this.lblPuntuacionMaxima.setText("Récord: " + this.recordAnterior);
-            this.lblPuntuacionMaxima.setIcon(imgCopa);
+        recordAnterior = controlGuardado.cargar();
+        if (recordAnterior != -1) {
+            lblPuntuacionMaxima.setText("Récord: " + recordAnterior);
+            lblPuntuacionMaxima.setIcon(imgCopa);
         }
     }
 
-    // Metodo principal.
+    // Main
     public static void main(String[] args) {
         new PantallaBienvenida();
     }
